771696f2d13d12462e2ba76f96ce780d7843bb28
Roberto Silva <beto-frs@hotmail.com>
2022-02-25 18:34:25 -0300

    Dúvida Douglas

---
+++ Testes/Aeroporto.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Testes
+{
+    public class Aeroporto
+    {
+        [JsonPropertyName("telImprensa")]
+        public string TelImprensa { get; set; }
+
+        [JsonPropertyName("City")]
+        public string City { get; set; }
+
+        [JsonPropertyName("Id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("Index")]
+        public int Index { get; set; }
+
+        [JsonPropertyName("Name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("numResults")]
+        public object NumResults { get; set; }
+
+        [JsonPropertyName("phone")]
+        public string Phone { get; set; }
+
+        [JsonPropertyName("SDU")]
+        public string SDU { get; set; }
+
+        [JsonPropertyName("State")]
+        public string State { get; set; }
+
+        [JsonPropertyName("telSuperintendente")]
+        public string TelSuperintendente { get; set; }
+
+        [JsonPropertyName("Url")]
+        public string Url { get; set; }
+
+        public async Task<List<Aeroporto>> GetAeroportos()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage();
+            request.RequestUri = new Uri("https://www4.infraero.gov.br/umbraco/surface/Common/index/");
+            request.Method = HttpMethod.Get;
+            request.Headers.Add("Accept", "*/*");
+            var response = await client.SendAsync(request);
+            var result = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<List<Aeroporto>>(result);
+        }
+
+        public async Task GetListAeroportosAsync(string estado)
+        {
+            foreach (var item in await GetAeroportos())
+            {
+                if (item.State == estado)
+                {
+                    Console.WriteLine($"Nome: {item.Name}\n" +
+                        $"SDU: {item.SDU}\n" +
+                        $"Cidade: {item.City}\n" +
+                        $"Estado: {item.State}\n" +
+                        $"Contato: {item.Phone}\n\n");
+                }
+            }
+        }
+
+        public async Task<List<string>> GetListStatesAsync()
+        {
+            List<string> listState = new();
+            foreach (var item in await GetAeroportos())
+            {
+                listState.Add(item.State);
+            }
+
+            return listState;
+        }
+    }
+}
---
+++ Testes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Testes
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Aeroporto aeroportos = new();
+            Console.WriteLine("MOSTRANDO AEROPORTOS\n\n");
+            Console.Write("Digite o estado com 2 dígitos:");
+            string escolha = Console.ReadLine().ToUpper();
+            var lista = aeroportos.GetListStatesAsync().Result;
+            if (lista.Contains(escolha) && escolha.Length == 2)
+            {
+                Console.WriteLine("\n======================PESQUISANDO OS DADOS======================\n");
+                await aeroportos.GetListAeroportosAsync(escolha);
+            }
+            else
+            {
+                Console.WriteLine("Dados inconsistentes... Favor, refaça a pesquisa");
+            }
+        }
+    }
+}
